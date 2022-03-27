0bdbef2c0fefbe1efa7c125f8318d83170613a57
pablito2199 <63094934+pablito2199@users.noreply.github.com>
2022-03-27 14:16:04 +0200

    Continuation

--- App/Cliente/src/App.js
+++ App/Cliente/src/App.js
@@ -35,17 +35,211 @@ export const App = () => {
                         <a>Diferenzas</a>
                         <a>Leis vinc.</a>
                     </nav>
+                    <div className="diferenzas">
+                        <div className="textoIzquierda">
+                            <select>
+                                <option>lex.gal</option>
+                                <option>DOG</option>
+                            </select>
+                            <p>
+                                Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+
+                                The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                                asdasdsadasdsa
+                            </p>
+                        </div>
+                        <div className="textoDerecha">
+                            <select>
+                                <option>lex.gal</option>
+                                <option>DOG</option>
+                            </select>
+                            <p>
+                                asdasdsadasdsa
+                            </p>
+                        </div>
+                    </div>
+                    <div className="leis_vinculadas"></div>
                 </section>
                 <section className="derecha">
                     <nav>
                         <a>Notas</a>
                         <a>Cambios</a>
                     </nav>
-                    <div className="botones">
-                        <Boton color="gris" texto="Aceptar selección" />
-                        <Boton color="gris" texto="Aceptar todos" />
-                        <Boton color="gris" texto="Descartar selección" />
-                        <Boton color="gris" texto="Descartar todos" />
+                    <div className="contenido">
+                        <p>
+                        </p>
+                        <div className="botones">
+                            <Boton color="gris" texto="Aceptar selección" />
+                            <Boton color="gris" texto="Aceptar todos" />
+                            <Boton color="gris" texto="Descartar selección" />
+                            <Boton color="gris" texto="Descartar todos" />
+                        </div>
                     </div>
                 </section>
             </main>
