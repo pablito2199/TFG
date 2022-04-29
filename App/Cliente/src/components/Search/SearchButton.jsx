c58485e6ec4d16d8c054990cc20742dfda2ef452
pablito2199 <63094934+pablito2199@users.noreply.github.com>
2022-04-29 12:59:46 +0200

    Update

--- App/Cliente/src/components/Search/SearchButton.jsx
+++ App/Cliente/src/components/Search/SearchButton.jsx
@@ -9,12 +9,6 @@ export const SearchButton = ({ texto, soloTitulo, fraseExacta, dogDesde, dogHast
         navigate(`/search?texto=${texto}&soloTitulo=${soloTitulo}&fraseExacta=${fraseExacta}&dogDesde=${dogDesde}&dogHasta=${dogHasta}&criterioOrdenacion=${criterioOrdenacion}&listado_colectivo=${colectivo}&listado_taxorga=${organizacion}&listado_rangos=${rango}&listado_seccion=${seccion}&listado_tematica=${tematica}&pagina=${pagina}`)
     }
 
-    if (texto === '') {
-        return <button className='focus:outline-none font-semibold px-6 py-2.5 bg-blue-green text-white hover:bg-blue-700 hover:shadow-lg transition duration-150 ease-in-out flex items-center' type="button" id="button-addon2">
-            <span>Buscar</span>
-        </button>
-    }
-
     return <button onClick={cambiarPagina} className='focus:outline-none font-semibold px-6 py-2.5 bg-blue-green text-white hover:bg-blue-700 hover:shadow-lg transition duration-150 ease-in-out flex items-center' type="button" id="button-addon2">
         <span>Buscar</span>
     </button >
